cb7e3532291a641e792f2c7a15e7e2e49cfba14a
Fakhir Shaukat <fakhirshaukat8@gmail.com>
2026-01-23 19:23:28 +0500

    Notes features and UI changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage";
 import Dashboard from "./pages/Dashboard";
+import AddNotes from "./components/AddNotes";
+import EditNotes from "./components/EditNotes";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/forgot" element={<ForgotPasswordPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/addnotes" element={<AddNotes />} />
+        <Route path="/editnotes" element={<EditNotes />} />
+        <Route path="/userprofile" element={<UserProfile />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/EditNotes.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+
+const EditNotes = ({ note, onClose, onUpdate, onDelete }) => {
+  const [title, setTitle] = useState(note.title);
+  const [description, setDescription] = useState(note.description);
+  const [deadline, setDeadline] = useState(note.deadline);
+
+  const handleUpdate = () => {
+    if (!title.trim()) return;
+    onUpdate({ ...note, title, description, deadline });
+    onClose();
+  };
+
+
+  return (
+    <div className="fixed inset-0 bg-black/40 flex justify-center items-center z-50">
+      <div className="bg-white w-[350px] rounded-lg p-4">
+        <h2 className="text-lg font-semibold mb-2">Edit Note</h2>
+
+        <input
+          type="text"
+          placeholder="Title"
+          className="border w-full p-2 mb-2"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+
+        <textarea
+          placeholder="Description"
+          className="border w-full p-2 h-24 resize-none mb-2"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Deadline"
+          className="border w-full p-2 mb-2 text-sm"
+          value={deadline}
+          onChange={(e) => setDeadline(e.target.value)}
+        />
+
+        <div className=" mt-3">
+          <div className="flex gap-2">
+            <button onClick={onClose} className="px-3 py-1 bg-red-500 rounded text-sm text-white">
+              Cancel
+            </button>
+            <button
+              onClick={handleUpdate}
+              className="px-3 py-1 bg-blue-500 text-white rounded text-sm"
+            >
+              Update
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditNotes;
