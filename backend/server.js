21eee2c04a9811e1207868ef58073e950cb935d6
Fakhir Shaukat <fakhirshaukat8@gmail.com>
2026-03-16 15:21:12 +0500

    ds

--- backend/server.js
+++ backend/server.js
@@ -14,12 +14,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/* REST APIs */
+/*REST APIs*/ 
 app.use("/api/notes", noteRoutes);
 app.use("/api/auth", authRoutes);
 
+
 app.get("/", (req, res) => {
-  res.send("Notes API running");
+  res.send("Notes API running ");
 });
 
-export default app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
